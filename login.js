8cb811b2f9cae84c79e3bbaf853501e336ec8c36
justtimi <139009300+justtimi@users.noreply.github.com>
2024-10-01 20:39:24 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,55 +1,53 @@
-const toggleBtn = document.querySelector('.nav-toggle');
-const Links = document.querySelector('.links');
-const DarkMode = document.getElementById('dark-modetoggle');
+const toggleBtn = document.querySelector(".nav-toggle");
+const Links = document.querySelector(".links");
+const DarkMode = document.getElementById("dark-modetoggle");
 
-window.addEventListener('DOMContentLoaded', function(){
-    const form = document.getElementById('myForm');
+window.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("myForm");
 
-    form.addEventListener('submit', function(e){
-
-        if (!validateForm()) {
-            e.preventDefault();
-        }
-    });
+  form.addEventListener("submit", function (e) {
+    if (!validateForm()) {
+      e.preventDefault();
+    }
+  });
 });
 
 function validateForm() {
-    const fields = [
-        {id: 'email', name: 'Email'},
-        {id: 'password', name: 'Password'},
-        
-    ];
-    let isValid = true;
-    fields.forEach(function(field){
-        const input = document.getElementById(field.id);
-        
-        const errorElement = document.getElementById(field.id + 'Error');
-        
-        errorElement.textContent = '';
-        errorElement.classList.remove('show');
+  const fields = [
+    { id: "email", name: "Email", validator: validateEmail },
+    { id: "password", name: "Password" },
+  ];
+  let isValid = true;
+  fields.forEach(function (field) {
+    const input = document.getElementById(field.id);
 
-        if (input.value.trim() === '') {
-            errorElement.textContent = `${field.name} cannot be empty`;
-            isValid = false;
-            errorElement.classList.add('show');
-        }
+    const errorElement = document.getElementById(field.id + "Error");
 
-        
-        
-    });
+    errorElement.textContent = "";
+    errorElement.classList.remove("show");
 
-    return isValid;
-};
+    if (input.value.trim() === "") {
+      errorElement.textContent = `${field.name} cannot be empty`;
+      isValid = false;
+      errorElement.classList.add("show");
+    } else if (field.validator && !field.validator(input.value)) {
+      errorElement.textContent = `Plese enter a valid ${field.name.toLowerCase()}`;
+      isValid = false;
+    }
+  });
 
-toggleBtn.addEventListener('click', function(){
-    Links.classList.toggle('show-links');
+  return isValid;
+}
+
+function validateEmail(email) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(String(email).toLowerCase());
+}
+
+toggleBtn.addEventListener("click", function () {
+  Links.classList.toggle("show-links");
 });
 
-DarkMode.addEventListener('click', function(){
-    document.body.classList.toggle('dark-mode');
-})
-
-
-
-
-
+DarkMode.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+});
